26152235fd7055f39e82c68fccc2bd556424696c
DanielaListh <dani_moe_berrios@hotmail.com>
2025-07-13 21:52:30 -0300

    toggle funciona?

--- js/menuResponsive.js
+++ js/menuResponsive.js
@@ -1,9 +1,19 @@
 //link para hacer funcionar al icono de hamburgesa para desplegar la barra de navegacion
 
-  const hamburgerBtn = document.getElementById('menu-icono');
-  const navLinks = document.getElementById('navLinks');
 
-  hamburgerBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-  });
+const menuIcon = document.getElementById("menu-icono");
+const navLinks = document.getElementById("navLinks");
+
+// Mostrar/ocultar menú al hacer clic en el ícono
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// Cerrar menú al hacer clic en una opción
+document.querySelectorAll("#navLinks a").forEach(link => {
+    link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+    });
+});
+
 
